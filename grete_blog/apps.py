490315dc0650ee1b470f18b6d37e6ed97f9ddfc0
iammburg <ilhamprabowo009@gmail.com>
2024-01-13 02:42:11 +0700

    first commit

---
+++ grete_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GreteBlogConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "grete_blog"
---
+++ grete_blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    content = models.TextField()
+
+    class Meta:
+        # define table name
+        db_table = "blog"
+
+    def save(self, *args, **kwargs):
+        # Membuat slug dari judul jika belum ada
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
